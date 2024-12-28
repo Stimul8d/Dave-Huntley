c72facd3fe932087f4f3c4dbf895b72ca3be4567
Stimul8d <davidhuntley.personal@googlemail.com>
2024-12-28 21:18:38 +0000

    fix: update site configuration and footer branding; adjust navbar and blog links to use base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,6 @@ import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://stimul8d.github.io",
-  base: import.meta.env.MODE === "production" ? "/Dave-Huntley" : "",
-  markdown: {
-    rehypePlugins: [
-      ['rehype-external-links', { target: '_blank', rel: ['noopener', 'noreferrer'] }]
-    ],
-  },
+  base: "/Dave-Huntley",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
